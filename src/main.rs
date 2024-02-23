764d05f5fc30fe30baea4fd98c73b23407a1c87b
lulalulo <lululo@tutanota.com>
2024-02-22 22:41:08 -0700

    app component

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use leptos::*;
 
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button 
+            on:click=move |_| {
+                // on stable, this is set_count.set(3);
+                set_count(3);
+            }
+        >
+            "Click me: "
+            // on stable, this is move || count.get();
+            {move || count()}
+        </button>
+    }
+}
+
 fn main() {
     mount_to_body(|| view! {|| view! { <App/> }})
 }
