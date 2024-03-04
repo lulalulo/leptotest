d12fc046ad8797d8a2949c5e9cb6fecb3ca2ed33
lulalulo <lululo@tutanota.com>
2024-03-03 18:24:40 -0700

    view

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,25 @@ fn App() -> impl IntoView {
         },
     ]);
     view! {
-
+        // when we click, update each row
+        // doubling its value
+        <button on:click=move |_| {
+            set_data.update(|data| {
+                for row in data {
+                    row.value *= 2;
+                }
+            });
+        }>
+            "Update Values"
+        </button>
+        // iterate over the rows and display each value
+        <For 
+            each=data
+            key=|state| state.key.clone()
+            let:child
+        >
+            <p>{child.value}</p>
+        </For>
     }
 }
 
