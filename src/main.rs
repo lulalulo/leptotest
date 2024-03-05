2873eb8dda3643bba54e9478a772e52ad31067ff
lulalulo <lululo@tutanota.com>
2024-03-05 15:53:42 -0700

    app changes

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,8 @@ use leptos::*;
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (value, set_value) = create_signal("B".to_string());
-    view! {
-        <select on:change=move |env| {
-            let new_value = event_target_value(&env);
-            set_value(new_value);
-        }>
-            <SelectOption value is="A"/>
-            <SelectOption value is="B"/>
-            <SelectOption value is="C"/>
-        </select>
+    view! { 
+        <NumericInput /> 
     }
 }
 
