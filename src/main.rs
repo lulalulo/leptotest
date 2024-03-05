139859d2ca991360540dbdc815865bb85a20462c
lulalulo <lululo@tutanota.com>
2024-03-05 15:07:42 -0700

    fix typo

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ pub fn App() -> impl IntoView {
 }
 
 #[component]
-pub fn SelectOption(is: &'static str, value: Readsignal<String>) -> impl IntoView {
+pub fn SelectOption(is: &'static str, value: ReadSignal<String>) -> impl IntoView {
     view! {
         <option
             value=is
