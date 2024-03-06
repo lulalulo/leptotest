f0041d2f1ac0e2cb57aece3b777b1d67cbfdf5e9
lulalulo <lululo@tutanota.com>
2024-03-05 21:15:14 -0700

    btn c comp eventlistener exmp

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ pub fn App() -> impl IntoView {
     let (toggled, set_toggled) = create_signal(false);
     view! {
         <p>"Toggled? " {toggled}</p>
-        <ButtonB on_click=move |_| set_toggled.update(|value| *value= !*value)/>
+        <ButtonC on:click=move |_| set_toggled.update(|value| *value= !*value)/>
     }
 }
 
@@ -43,6 +43,13 @@ where
         <button on:click=on_click>
             "Toggle"
         </button>
+    }
+}
+
+#[component]
+pub fn ButtonC() -> impl IntoView {
+    view! {
+        <button>"Toggle"</button>
     }
 }
 
