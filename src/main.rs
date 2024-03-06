4e9bf293293c1728a2354d71d4178361f40f1e3e
lulalulo <lululo@tutanota.com>
2024-03-05 20:24:00 -0700

    btn comp

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,13 @@ pub fn App() -> impl IntoView {
 }
 
 #[component]
-pub fn NumericInput() -> impl IntoView {
-    let (value, set_value) = create_signal(Ok(0));
-
-    // when input changes, try to parse a number from the input
-    let on_input = move |ev| set_value(event_target_value(&ev).parse::<i32>());
-
+pub fn ButtonA(setter: WriteSignal<bool>) -> impl IntoView {
     view! {
-        <label>
-            "Type a number"
-            <input type="number" on:inout=on_input/>
-            <p>
-                "You entered "
-                <strong>{value}</strong>
-            </p>
-        </label>
+        <button
+            on:click=move |_| setter.update(|value| *value = !*value)
+        >
+            "Toggle"
+        </button>
     }
 }
 
